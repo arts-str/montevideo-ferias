0cf96cd1ddf679074e9ac2befe85cd1ff6f4fc72
ARTS-str <magninfacundo@gmail.com>
2025-10-14 12:48:32 -0300

    Cambios de colores

--- js/script.js
+++ js/script.js
@@ -9,6 +9,21 @@ document.addEventListener('touchmove', function (e) {
   
 const searchInput = document.querySelector('nav input');
 
+
+
+    let r = document.querySelector(':root');
+    let rs = getComputedStyle(r);
+
+    let colorL = rs.getPropertyValue('--light-bg');
+    let colorHoverL = rs.getPropertyValue('--light-bg-hover');
+    let colorActiveL = rs.getPropertyValue('--light-bg-active');
+
+    let colorD = rs.getPropertyValue('--dark-bg');
+    let colorHoverD = rs.getPropertyValue('--dark-bg-hover');
+    let colorActiveD = rs.getPropertyValue('--dark-bg-active');
+
+
+
 searchInput.addEventListener('focus', () => {
   // Lock the page width to prevent iOS from expanding layout
   document.body.style.position = 'fixed';
@@ -32,7 +47,7 @@ function addMunicipio() {
             <div class="municipio-container">
                 <div onclick="desplegar(this, true)" class="municipio inactive municipio-head color-mode-transition">
                     <h3>Municipio ${municipio.nombre}</h3>
-                    <svg width="1.5rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z" fill="#0F0F0F"></path> </g></svg>    
+                    <svg width="1.5rem" viewBox="0 0 24 24" class="arrowSvg color-mode-transition" fill="${isDark ? colorD:colorL}" xmlns="http://www.w3.org/2000/svg"> <path d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"></path></svg>    
                 </div>
                 <div class="contents" id="contenido">
                     ${addBarrios(municipio)}
@@ -134,43 +149,44 @@ function buscar(allFerias, query) {
 }
 
 
+
 function updateColors() {
 
     let logo = document.getElementById('logo');
-
-    console.log(logo, isDark);
+    let arrowSvg = document.querySelectorAll('.arrowSvg');
+    let nav = document.getElementById('nav');
     let imgSrc = (isDark) ? './assets/mf-logo-dark-42.png':'./assets/mf-logo-light-42.png';
     logo.src = imgSrc;
 
-    let r = document.querySelector(':root');
-    let rs = getComputedStyle(r);
     let currentColor = rs.getPropertyValue('--bg');
-    
-    let currentColorL = rs.getPropertyValue('--light-bg');
-    let currentColorHoverL = rs.getPropertyValue('--light-bg-hover');
-    let currentColorActiveL = rs.getPropertyValue('--light-bg-active');
 
-    
-    let currentColorD = rs.getPropertyValue('--dark-bg');
-    let currentColorHoverD = rs.getPropertyValue('--dark-bg-hover');
-    let currentColorActiveD = rs.getPropertyValue('--dark-bg-active');
-    
     let elements = document.querySelectorAll('.color-mode-transition');
     elements.forEach(element => {
-        element.style.transition = 'background-color 200ms ease-in-out, outline 200ms ease-in-out';
+        element.style.transition = 'background-color 200ms ease-in-out, outline 200ms ease-in-out, box-shadow 200ms ease-in-out, color 200ms ease-in-out, fill 200ms ease-in-out, stroke 200ms ease-in-out';
         setTimeout(() => {
-            
             element.style.transition = 'none';
         }, 200);
     });
-    if (currentColor === currentColorL) {
-        r.style.setProperty('--bg', currentColorD);
-        r.style.setProperty('--bg-hover', currentColorHoverD);
-        r.style.setProperty('--bg-active', currentColorActiveD);
+    if (currentColor === colorL) {
+        menu.style.setProperty('color', colorL);
+        arrowSvg.forEach(arrow => {
+            arrow.setAttribute('fill', colorL);
+        });
+        nav.style.setProperty('box-shadow', '0px -2px 5px ' + colorL);
+        userLocate.children[0].setAttribute('stroke', colorL);
+        r.style.setProperty('--bg', colorD);
+        r.style.setProperty('--bg-hover', colorHoverD);
+        r.style.setProperty('--bg-active', colorActiveD);
     }else{
-        r.style.setProperty('--bg', currentColorL);
-        r.style.setProperty('--bg-hover', currentColorHoverL);
-        r.style.setProperty('--bg-active', currentColorActiveL);
+        menu.style.setProperty('color', colorD);
+        arrowSvg.forEach(arrow => {
+            arrow.setAttribute('fill', colorD);
+        });
+        nav.style.setProperty('box-shadow', '0px 0px 5px ' + colorD);
+        userLocate.children[0].setAttribute('stroke', colorD);
+        r.style.setProperty('--bg', colorL);
+        r.style.setProperty('--bg-hover', colorHoverL);
+        r.style.setProperty('--bg-active', colorActiveL);
     }
 }
 updateColors()
